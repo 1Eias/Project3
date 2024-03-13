760e1118ec85b65fc79ed48f34e23ea62d42361c
Eias <eiasbilalsy@gmail.com>
2024-03-13 12:09:53 +0100

    styling

--- index.php
+++ index.php
@@ -14,14 +14,31 @@
     }
     ?>
 
-<div class="grid-container">
-    <div class="main">
-        <p class="page_title">Home</p>
+    <div class="grid-container">
+        <div class="main">
+            <p class="page_title">Home</p>
 
-        <div class="tweet_box_tweet_add">
-            <div class="tweet_left">
-                <img src="images/avatar.png" alt="">
+            <div class="tweet_box_tweet_add">
+                <div class="tweet_left">
+                    <img src="images/avatar.png" alt="">
+                </div>
+
+                <div class="tweet_body">
+                    <form method="post" enctype="multipart/form-data">
+                        <textarea name="post_text" id="" cols="100%" rows="3"
+                            placeholder="What's happening?"></textarea>
+                        <div class="tweet_icons_wrapper">
+                            <div class="tweet icons add">
+                                <i class="far fa-image"></i>
+                                <i class="fa fa-chart-bar"></i>
+                                <i class="far fa-smile"></i>
+                                <i class="far fa-calendar-alt"></i>
+                            </div>
+                            <button class="button_tweet" type="submit" name="btn_add_post">Tweet</button>
+                        </div>
+                    </form>
+                </div>
             </div>
+            <?php require_once "tweet.php"; ?>
         </div>
     </div>
-</div>
