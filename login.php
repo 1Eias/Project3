5196d1cf89a469e91aabdfd253d644c0e4aff1ae
Ni-Yara <9021288@student.zadkine.nl>
2024-03-13 14:43:40 +0100

    update login page

--- login.php
+++ login.php
@@ -9,10 +9,9 @@
     <script src="https://kit.fontawesome.com/2f6b0c498f.js" crossorigin="anonymous"></script>
     <title>Inloggen / Chirpify</title>
 </head>
-
 <body>
-<div class="chirpify" id="blur">
-<div class="login-box" >
+<div class="chirpify" >
+<div class="login-box" id="blur">
     <div class="login-content">
         <h1 class="big-login">Nu registreren</h1>
         <button class="regi-google"><img src="images/google.png" alt="">Registeren met Google</button>
@@ -22,12 +21,20 @@
         <button type="submit" class="account-registration">Account aanmaken</button>
         <br>
         <h3>Heb je al een account?</h3>
-        <button type="submit" class="login">Inloggen</button>
+        <button type="submit" class="login" onclick="toggle()">Inloggen</button>
     </div>
 </div>
-<div class="login-popup">
-    <div class="login-popup-content" id="popup">
-        <form action="">
+    <script type="text/javascript">
+        function toggle() {
+            let blur = document.getElementById('blur')
+            blur.classList.toggle('active')
+            let popup = document.getElementById('popup')
+            popup.classList.toggle('active')
+        }
+    </script>
+<div class="login-popup" id="popup">
+        <a href="login.php"><label for="" class="close-btn fa-solid fa-xmark"></label></a>
+        <form  action="">
             <i class="fa-solid fa-dove"></i>
         <h2 class="chirp-login">Login</h2>
         <label for="username">
@@ -43,7 +50,6 @@
         <button class="forget-password">Wachtwoord vergeten?</button>
         <p class="no-account">Heb je geen account? <a class="no-account" href="#">Registreren</a></p>
         </form>
-    </div>
 </div>
 </div>
 </body>
